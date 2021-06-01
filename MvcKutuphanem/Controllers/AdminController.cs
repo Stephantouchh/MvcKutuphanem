417e61ad5238686694fe187964a5775aab274450
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2021-06-02 00:20:59 +0300

    initial commit

--- MvcKutuphanem/Controllers/AdminController.cs
+++ MvcKutuphanem/Controllers/AdminController.cs
@@ -11,7 +11,7 @@ namespace MvcKutuphanem.Controllers
     [AllowAnonymous]
     public class AdminController : Controller
     {
-        // GET: Admin
+        // GET: Admintest
         DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
         public ActionResult Login()
         {
